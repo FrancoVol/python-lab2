477033ae45069fbcf880ab16af7ec33a06e23ca0
FrancoVol <s233868@studenti.polito.it>
2019-03-18 16:05:09 +0100

    Last commit, all complete

--- lab2ex4.py
+++ lab2ex4.py
@@ -4,4 +4,15 @@ task3 = {"todo": "find a present for Angelina’s birthday", "urgent": False}
 task4 = {"todo": "organize mega party (last week of April)", "urgent": False}
 task5 = {"todo": "book summer holidays", "urgent": False}
 task6 = {"todo": "whatsapp Mary for a coffee", "urgent": False}
+truetask=[]
+i=0
+tasks=[task1,task2,task3,task4,task5,task6]
+for search in tasks:
+    if search["urgent"]==True:
+        truetask.append(search)
+        print ("Task {} = {}".format(i+1,truetask[i]))
+        i+=1
+
+
+
 
